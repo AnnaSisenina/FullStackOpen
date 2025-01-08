8dc6d0be66564abea16e5455f21cba9230a6cbef
Anna Sisenina <AG1932@student.jamk.fi>
2025-01-08 23:11:46 +0200

    introdemo - handling arrays example

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -1,23 +1,32 @@
 import { useState } from "react"
 
-const Display = ({left, right}) => <div>left: {left} right: {right}</div>
+const Display = ({left, right, allClicks}) => <div>left: {left} right: {right} <p>{allClicks.join(" ")}</p></div>
 
 const Button = ({onClick, text}) => <button onClick={onClick}> {text}</button>
 
 
 const App = () => {
   const [clicks, setClicks] = useState({left: 0, right: 0})
+  const [allClicks, setAll] = useState([]) 
   
 
-  const addLeft = () => setClicks({...clicks, left: clicks.left + 1})
-  const addRight = () => setClicks({...clicks, right: clicks.right + 1})
+  const addLeft = () => {
+    setClicks({...clicks, left: clicks.left+1})
+    setAll(allClicks.concat('L'))
+    }
   
+  const addRight = () => {
+    setClicks({...clicks, right: clicks.right+1})
+    setAll(allClicks.concat('R'))
+  } 
+    
 
   return (
       <div>
         <Display 
         left = {clicks.left}
-        right={clicks.right}/>
+        right={clicks.right}
+        allClicks={allClicks}/>
         <Button
         text='left'
         onClick={addLeft}
