651d1d0d6a2439b80ece02a2dadae7364fdb83f2
Anna Sisenina <AG1932@student.jamk.fi>
2025-01-09 02:29:35 +0200

    ex. 1.9-1.10 done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,15 +5,30 @@ const Button = ({text, onClick}) => {
     <button onClick={onClick}>{text}</button>
   )
 }
+const StatisticsLine = ({text, value}) => {
+  return(
+    <p>{text}: {value}</p>
+  )
+}
 
 const DisplayStat = ({good, neutral, bad, total, average, positive}) => {
+  if (total === 0){
+    return(
+      <div>
+        <h3>Statistics:</h3>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
   return(
     <div>
       <h3>Statistics:</h3>
-      <p>Good: {good} Neutral: {neutral}  Bad: {bad}</p>
-      <p>Total: {total}</p> 
-      <p>Average: {average}</p> 
-      <p>Positive: {positive}%</p>
+      <StatisticsLine text="Good" value={good}/>
+      <StatisticsLine text="Neutral" value={neutral}/>
+      <StatisticsLine text="Bad" value={bad}/>
+      <StatisticsLine text="Total" value={total}/>
+      <StatisticsLine text="Average" value={`${average.toFixed(2)}`}/>
+      <StatisticsLine text="Positive" value={`${positive.toFixed(2)}%`}/>
     </div>
   )
 }
