114e839c375e3d84934a4a40988a2ca21a70249f
Anna Sisenina <AG1932@student.jamk.fi>
2025-01-09 02:01:12 +0200

    ex. 1.7-1.8 done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,13 +6,14 @@ const Button = ({text, onClick}) => {
   )
 }
 
-const DisplayStat = ({good, neutral, bad}) => {
+const DisplayStat = ({good, neutral, bad, total, average, positive}) => {
   return(
     <div>
       <h3>Statistics:</h3>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
+      <p>Good: {good} Neutral: {neutral}  Bad: {bad}</p>
+      <p>Total: {total}</p> 
+      <p>Average: {average}</p> 
+      <p>Positive: {positive}%</p>
     </div>
   )
 }
@@ -21,10 +22,36 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
 
-  const setToGood = () => setGood(good+1)
-  const setToNeutral = () => setNeutral(neutral+1)
-  const setToBad = () => setBad(bad+1)
+  const setToGood = () => {
+    const newGoodValue = good + 1
+    setGood(newGoodValue)
+    const newTotal = newGoodValue + neutral + bad 
+    setTotal(newTotal)
+    setAverage((newGoodValue*1 + neutral*0 + bad*(-1)) / newTotal)
+    setPositive(newGoodValue * 100 / newTotal)
+  }
+
+  const setToNeutral = () => {
+    const newNeutralValue = neutral + 1
+    setNeutral(newNeutralValue)
+    const newTotal = good + newNeutralValue + bad 
+    setTotal(newTotal)
+    setAverage((good*1 + newNeutralValue*0 + bad*(-1)) / newTotal)
+    setPositive(good * 100 / newTotal)
+  }
+
+  const setToBad = () => {
+    const newBadValue = bad + 1
+    setBad(newBadValue)
+    const newTotal = good + neutral + newBadValue 
+    setTotal(newTotal)
+    setAverage((good*1 + neutral*0 + newBadValue*(-1)) / newTotal)
+    setPositive(good * 100 / newTotal)
+  }
 
   return (
     <div>
@@ -41,7 +68,10 @@ const App = () => {
       <DisplayStat
       good = {good}
       neutral={neutral}
-      bad={bad}/>
+      bad={bad}
+      total={total}
+      average={average}
+      positive={positive}/>
     </div>
   )
 }
