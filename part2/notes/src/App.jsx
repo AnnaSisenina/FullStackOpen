c114c8716cb0288788464440af8d58c522066351
Anna Sisenina <AG1932@student.jamk.fi>
2025-01-21 12:16:58 +0200

    Part2 - Notes Adding styles to React app)

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -2,11 +2,14 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import Note from "./components/Note";
 import noteService from "./services/notes";
+import Notification from "./components/Notification";
+import Footer from "./components/Footer";
 
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("a new note...");
   const [showAll, setShowAll] = useState(true);
+  const [errorMessage, setErrorMessage] = useState("some error happened...")
   
   const toggleImportanceOf = (id) => {
     const note = notes.find(n => n.id === id)
@@ -18,7 +21,12 @@ const App = () => {
       setNotes(notes.map(n => n.id === id ? returnedNote : n))
     })
     .catch(error => {
-      alert(`the note '${note.content}' was already deleted from server`);
+      setErrorMessage(
+        `Note '${note.content}' was already removed from server`
+      )
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
       setNotes(notes.filter(n => n.id !== id));
   })
 }
@@ -61,6 +69,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <ul>
         {notesToShow.map((note) => (
           <Note key={note.id} note={note} toggleImportance={() => toggleImportanceOf(note.id)} />
@@ -74,6 +83,7 @@ const App = () => {
         {" "}
         show {showAll ? "important" : "all"}{" "}
       </button>
+      <Footer />
     </div>
   );
 };
